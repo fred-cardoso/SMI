02c3b045950baf24289ab5f5c8f34498eafc79fd
Frederico Cardoso <fredecardoso@hotmail.com>
2019-04-30 19:48:55 +0100

    Create model Categoria

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Projeto SMI'),
+    'name' => env('APP_NAME', 'ProjetoSMI'),
 
     /*
     |--------------------------------------------------------------------------
