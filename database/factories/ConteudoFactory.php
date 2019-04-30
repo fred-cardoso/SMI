b27615fda8969d240ea8d8b640437734ece37029
LonelyCustom <claudionorpereira1997@hotmail.com>
2019-04-30 21:20:18 +0100

    AdicionarControlador

---
+++ database/factories/ConteudoFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Conteudo;
+use Faker\Generator as Faker;
+
+$factory->define(Conteudo::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
