2b7c5326249bc763789b8028510ea1c0c4a7f9e5
Frederico Cardoso <fredecardoso@hotmail.com>
2019-04-30 21:29:19 +0100

    .gitignore Update

--- database/migrations/2019_04_30_184716_create_categorias_table.php
+++ database/migrations/2019_04_30_184716_create_categorias_table.php
@@ -16,7 +16,7 @@ class CreateCategoriasTable extends Migration
         Schema::create('categorias', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('nome');
-            $table->boolean("secundaria");
+            $table->boolean("secundária");
             $table->timestamps();
         });
     }
