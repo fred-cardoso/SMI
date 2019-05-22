44c6945302b93c500704f50f6715ff1a38c14cda
LonelyCustom <claudionorpereira1997@hotmail.com>
2019-05-22 18:49:44 +0100

    Subscribe

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -2,13 +2,27 @@
 @section('title', 'Utilizadores')
 @section('content')
     //TODO: Sucesso ou erro ao eliminar users
+
     <?php
+    $role = Auth::user()->roles->first();
     foreach ($users as $user) {
-        echo '<a href="users/' . $user->id . '/edit">' . $user->name . '</a>&nbsp;';
-        echo '<form action="users/' . $user->id . '/delete" method="POST">';
-        ?>@csrf<?php
-        echo '<input type="submit" value="Eliminar"></form>';
-        echo '</br>';
+
+
+    echo '<a href="users/' . $user->id . '/edit">' . $user->name . '</a>&nbsp;';
+    echo '<form action="users/' . $user->id . '/delete" method="POST">';
+    ?>@csrf<?php
+
+    if ($role->slug == 'admin'){
+    echo '<input type="submit" value="Eliminar"></form>';
+    }
+    echo '</br>';
+    echo '</form>';
+    echo '<form action="users/' . $user->id . '/subscribe" method="POST">';
+    echo '<input type="submit" value="Subscrever"></form>' ;
+    echo '</form>';
+
+
     }
     ?>
+
 @endsection
