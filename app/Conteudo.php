42edeb4f56d5e3780e696723ee899f563c278909
LonelyCustom <claudionorpereira1997@hotmail.com>
2019-05-22 17:37:40 +0100

    Better Filiable

--- app/Conteudo.php
+++ app/Conteudo.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Conteudo extends Model
 {
     protected $fillable = [
-        'titulo','tipo','nome','utilizador','categoria','tag'
+        'titulo','tipo','nome','descricao','privado','user_id'
     ];
 
     public function user(){
