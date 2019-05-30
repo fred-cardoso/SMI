adbf783fcce1765c899531b43b8ca1784a572ce0
Frederico Cardoso <fredecardoso@hotmail.com>
2019-05-30 17:27:44 +0100

    Rename ConfigurationsController

--- app/Http/Controllers/ConfigurationsController.php
+++ app/Http/Controllers/ConfigurationsController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class Configurations extends Controller
+class ConfigurationsController extends Controller
 {
     public function setEnv($name, $value)
     {
