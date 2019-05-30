a0829b00cb39b0bb8d3e86b32d4073794d21aff4
Frederico Cardoso <fredecardoso@hotmail.com>
2019-05-31 00:24:41 +0100

    Minor Fixes

--- app/Conteudo.php
+++ app/Conteudo.php
@@ -13,11 +13,17 @@ class Conteudo extends Model
     public function user(){
         return $this->belongsTo("\App\User");
     }
+
     public function category(){
         return $this->belongsToMany(Categoria::class, "conteudos_categorias");
     }
 
-    public function hasCategory($category){
-        return $this->category()->where('id', $category->id)->exists();
+    public function hasCategory(... $categories){
+        foreach ($categories as $category) {
+            if ($this->category()->where('id', $category->id)->exists()) {
+                return true;
+            }
+        }
+        return false;
     }
 }
