888772fd8894782509465b6156d05f636944c290
Frederico Cardoso <fredecardoso@hotmail.com>
2019-05-30 18:29:00 +0100

    Changed app language

--- config/app.php
+++ config/app.php
@@ -80,7 +80,7 @@ return [
     |
     */
 
-    'locale' => 'en',
+    'locale' => 'pt',
 
     /*
     |--------------------------------------------------------------------------
@@ -161,6 +161,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        yasmuru\LaravelTinify\LaravelTinifyServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -226,6 +227,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Tinify' => yasmuru\LaravelTinify\Facades\Tinify::class,
 
     ],
 
