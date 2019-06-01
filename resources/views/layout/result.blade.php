ec2c307e8069d6d6875845ae08ffb19865e7083e
Frederico Cardoso <fredecardoso@hotmail.com>
2019-06-01 19:29:12 +0100

    Update result blade

--- resources/views/layout/result.blade.php
+++ resources/views/layout/result.blade.php
@@ -1,5 +1,6 @@
 @if ($errors->any())
     <div class="alert alert-danger alert-dismissible">
+        <h4><i class="icon fa fa-ban"></i> Atenção!</h4>
         <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
         <ul>
             @foreach ($errors->all() as $error)
@@ -10,6 +11,7 @@
 @endif
 @if(session('success'))
     <div class="alert alert-success alert-dismissible">
+        <h4><i class="icon fa fa-check"></i> Sucesso!</h4>
         <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
         {{session('success')}}
     </div>
