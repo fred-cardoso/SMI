9c726d0bdbe1cd6dc39e764efe784ed928355d2e
LonelyCustom <claudionorpereira1997@hotmail.com>
2019-06-10 18:49:57 +0100

    Install multi langue

---
+++ resources/lang/pt/install.php
@@ -0,0 +1,48 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+    'php_version' => 'Versão do PHP >= 7.1.3',
+    'BCMath' => 'Extensão BCMath',
+    'CType' => 'Extensão CType',
+    'JSON' => 'Extensão JSON',
+    'MBString' => 'Extensão MBString',
+    'OpenSSL' => 'Extensão OpenSSL',
+    'PDO' => 'Extensão PDO',
+    'tokenizer' => 'Extensão Tokenizer',
+    'XML' => 'Extensão XML',
+    'storage' =>"Escrita em 'storage'",
+    'storage_logs' =>"Escrita em 'storage/logs'",
+    'storage_app' =>"Escrita em 'storage/app'",
+    'storage_framework' =>"Escrita em 'storage/framework'",
+    'app_address' => 'Endereço da Aplicação (http://nome.com)',
+    'server_address' =>'Endereço do Servidor',
+    'server_port' =>'Porto do Servidor',
+    'db_name' =>'Nome da Base de Dados',
+    'sql_user' =>'Utilizador SQL',
+    'sql_password' =>'Password SQL',
+    'smtp_user' =>'Utilizador SMTP',
+    'smtp_password' =>'Password SMTP',
+    'name' =>'Nome ',
+    'email' =>'Email ',
+    'password' =>'Password ',
+    'confirm_pass' =>'Confirme password',
+    'email_reme' =>'Email Remetente (email@website.com)'
+
+
+
+
+
+
+
+];
